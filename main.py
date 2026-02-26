7a5319851dbabed64fb848379d81784562493d46
Lionel JOURDHIER <lio.jourdhier@gmail.com>
2026-02-26 15:43:11 +0100

    Reponses

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print("Hello from tuto-branching-vscode!")
     print("bonjour Antony")
     print("hello")
+    print("bye")
+    print("merci")
 
 if __name__ == "__main__":
     main()
